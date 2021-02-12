2386b5208510e471128a914c6ca573a8ccc1d28c
soleil-taruto <soleil.taruto@gmail.com>
2021-02-13 08:46:51 +0900

    Backup 20210213084645

--- e20201301_NovelAdv_Base/Elsa20200001/Elsa20200001/Commons/SCommon.cs
+++ e20201301_NovelAdv_Base/Elsa20200001/Elsa20200001/Commons/SCommon.cs
@@ -335,9 +335,9 @@ namespace Charlotte.Commons
 					{
 						File.Delete(path);
 					}
-					catch (Exception e)
+					catch //(Exception e)
 					{
-						ProcMain.WriteLog(e + " <---- 例外ここまで、処理を続行します。");
+						//ProcMain.WriteLog(e + " <---- 例外ここまで、処理を続行します。");
 					}
 					if (!File.Exists(path))
 						break;
@@ -357,9 +357,9 @@ namespace Charlotte.Commons
 					{
 						Directory.Delete(path, true);
 					}
-					catch (Exception e)
+					catch //(Exception e)
 					{
-						ProcMain.WriteLog(e + " <---- 例外ここまで、処理を続行します。");
+						//ProcMain.WriteLog(e + " <---- 例外ここまで、処理を続行します。");
 					}
 					if (!Directory.Exists(path))
 						break;
@@ -384,9 +384,9 @@ namespace Charlotte.Commons
 				{
 					Directory.CreateDirectory(dir); // ディレクトリが存在するときは何もしない。
 				}
-				catch (Exception e)
+				catch //(Exception e)
 				{
-					ProcMain.WriteLog(e + " <---- 例外ここまで、処理を続行します。");
+					//ProcMain.WriteLog(e + " <---- 例外ここまで、処理を続行します。");
 				}
 				if (Directory.Exists(dir))
 					break;
