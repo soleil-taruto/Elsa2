c5c204091ce8eee3447bd2b4cc62e3810ba96fd9
soleil-taruto <soleil.taruto@gmail.com>
2021-02-26 19:39:44 +0900

    Backup 20210226193921

--- e20210261_SSGame/Upload.bat
+++ e20210261_SSGame/Upload.bat
@@ -6,4 +6,4 @@ rem	CALL DebugRelease.bat /B
 rem	CALL Release.bat /V 001
 
 C:\Factory\Petra\UpdatingCopy.exe out C:\be\Web\DocRoot\Elsa2\d20210261_SSGame
-C:\Factory\Petra\RunOnBatch.exe C:\be\Web Deploy.bat
+C:\Factory\Petra\RunOnBatch.exe C:\be\Web Upload.bat
