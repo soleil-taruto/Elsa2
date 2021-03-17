d66d000d96a6404e5b9831ff1b9e716570e041e0
soleil-taruto <soleil.taruto@gmail.com>
2021-03-17 22:37:35 +0900

    Backup 20210317223725

---
+++ e20210227_SSGame2/Elsa20200001/Elsa20200001/Games/TitleMenu.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Commons;
+using Charlotte.GameCommons;
+using Charlotte.Novels;
+
+namespace Charlotte.Games
+{
+	public class TitleMenu : IDisposable
+	{
+		public static TitleMenu I;
+
+		public TitleMenu()
+		{
+			I = this;
+		}
+
+		public void Dispose()
+		{
+			I = null;
+		}
+
+		private DDSimpleMenu SimpleMenu;
+
+		public void Perform()
+		{
+			DDCurtain.SetCurtain(0, -1.0);
+			DDCurtain.SetCurtain();
+
+			DDEngine.FreezeInput();
+
+			Ground.I.Music.Title.Play();
+
+			string[] items = new string[]
+			{
+				"ゲームスタート",
+				"コンテニュー",
+				"設定",
+				"終了",
+			};
+
+			int selectIndex = 0;
+
+			this.SimpleMenu = new DDSimpleMenu();
+
+			this.SimpleMenu.BorderColor = new I3Color(64, 0, 0);
+			//this.SimpleMenu.WallColor = new I3Color(130, 120, 0);
+			this.SimpleMenu.WallPicture = Ground.I.Picture.Title;
+			this.SimpleMenu.WallCurtain = -0.4;
+
+			for (; ; )
+			{
+				selectIndex = this.SimpleMenu.Perform("横スクロール アクションゲーム タイプ-M テストコード / タイトルメニュー", items, selectIndex);
+
+				switch (selectIndex)
+				{
+					case 0:
+						{
+							this.LeaveTitleMenu();
+
+							using (new WorldGameMaster())
+							{
+								WorldGameMaster.I.World = new World("t0001"); // マップ名_仮？
+								WorldGameMaster.I.Status = new GameStatus();
+								WorldGameMaster.I.Perform();
+							}
+							this.ReturnTitleMenu();
+						}
+						break;
+
+					case 1:
+						{
+							Ground.P_SaveDataSlot saveDataSlot = LoadGame();
+
+							if (saveDataSlot != null)
+							{
+								this.LeaveTitleMenu();
+
+								using (new WorldGameMaster())
+								{
+									WorldGameMaster.I.World = new World(saveDataSlot.MapName);
+									WorldGameMaster.I.Status = saveDataSlot.GameStatus.GetClone();
+									WorldGameMaster.I.Status.StartPointDirection = 101; // スタート地点を「ロード地点」にする。
+									WorldGameMaster.I.Perform();
+								}
+								this.ReturnTitleMenu();
+							}
+						}
+						break;
+
+					case 2:
+						this.Setting();
+						break;
+
+					case 3:
+						goto endMenu;
+
+					default:
+						throw new DDError();
+				}
+			}
+		endMenu:
+			DDMusicUtils.Fade();
+			DDCurtain.SetCurtain(30, -1.0);
+
+			foreach (DDScene scene in DDSceneUtils.Create(40))
+			{
+				this.SimpleMenu.DrawWall();
+				DDEngine.EachFrame();
+			}
+
+			DDEngine.FreezeInput();
+		}
+
+		private static Ground.P_SaveDataSlot LoadGame() // ret: null == キャンセル, ret.GameStatus を使用する際は GetClone を忘れずに！
+		{
+			Ground.P_SaveDataSlot saveDataSlot = null;
+
+			DDEngine.FreezeInput();
+
+			DDCurtain.SetCurtain();
+			DDEngine.FreezeInput();
+
+			DDSimpleMenu simpleMenu = new DDSimpleMenu();
+
+			simpleMenu.BorderColor = new I3Color(0, 128, 0);
+			simpleMenu.WallColor = new I3Color(64, 64, 128);
+
+			string[] items = Ground.I.SaveDataSlots.Select(v => v.GameStatus == null ?
+				"----" :
+				"[" + v.TimeStamp + "]　" + v.Description).Concat(new string[] { "戻る" }).ToArray();
+
+			int selectIndex = 0;
+
+			for (; ; )
+			{
+				selectIndex = simpleMenu.Perform("ロード画面", items, selectIndex);
+
+				if (selectIndex < Consts.SAVE_DATA_SLOT_NUM)
+				{
+					if (Ground.I.SaveDataSlots[selectIndex].GameStatus != null)
+					{
+						if (new Confirm() { BorderColor = new I3Color(50, 100, 200) }
+							.Perform("スロット " + (selectIndex + 1) + " のデータをロードします。", "はい", "いいえ") == 0)
+						{
+							saveDataSlot = Ground.I.SaveDataSlots[selectIndex];
+							break;
+						}
+					}
+				}
+				else // [戻る]
+				{
+					break;
+				}
+				//DDEngine.EachFrame(); // 不要
+			}
+			DDEngine.FreezeInput();
+
+			return saveDataSlot;
+		}
+
+		private void Setting()
+		{
+			DDCurtain.SetCurtain();
+			DDEngine.FreezeInput();
+
+			string[] items = new string[]
+			{
+				"ゲームパッドのボタン設定",
+				"キーボードのキー設定",
+				"ウィンドウサイズ変更",
+				"ＢＧＭ音量",
+				"ＳＥ音量",
+				"ノベルパートのメッセージ表示速度",
+				"戻る",
+			};
+
+			DDSE[] seSamples = new DDSE[]
+			{
+				Ground.I.SE.EnemyDamaged,
+				Ground.I.SE.EnemyKilled,
+			};
+
+			int selectIndex = 0;
+
+			for (; ; )
+			{
+				selectIndex = this.SimpleMenu.Perform("設定", items, selectIndex);
+
+				switch (selectIndex)
+				{
+					case 0:
+						this.SimpleMenu.PadConfig();
+						break;
+
+					case 1:
+						this.SimpleMenu.PadConfig(true);
+						break;
+
+					case 2:
+						this.SimpleMenu.WindowSizeConfig();
+						break;
+
+					case 3:
+						this.SimpleMenu.VolumeConfig("ＢＧＭ音量", DDGround.MusicVolume, 0, 100, 1, 10, volume =>
+						{
+							DDGround.MusicVolume = volume;
+							DDMusicUtils.UpdateVolume();
+						},
+						() => { }
+						);
+						break;
+
+					case 4:
+						this.SimpleMenu.VolumeConfig("ＳＥ音量", DDGround.SEVolume, 0, 100, 1, 10, volume =>
+						{
+							DDGround.SEVolume = volume;
+							//DDSEUtils.UpdateVolume(); // old
+
+							foreach (DDSE se in seSamples) // サンプルのみ音量更新
+								se.UpdateVolume();
+						},
+						() =>
+						{
+							DDUtils.Random.ChooseOne(seSamples).Play();
+						}
+						);
+						DDSEUtils.UpdateVolume(); // 全音量更新
+						break;
+
+					case 5:
+						this.SimpleMenu.IntVolumeConfig(
+							"ノベルパートのメッセージ表示速度",
+							Ground.I.NovelMessageSpeed,
+							NovelConsts.MESSAGE_SPEED_MIN,
+							NovelConsts.MESSAGE_SPEED_MAX,
+							1,
+							2,
+							speed => Ground.I.NovelMessageSpeed = speed,
+							() => { }
+							);
+						break;
+
+					case 6:
+						goto endMenu;
+
+					default:
+						throw new DDError();
+				}
+			}
+		endMenu:
+			DDEngine.FreezeInput();
+		}
+
+		private void LeaveTitleMenu()
+		{
+			DDMusicUtils.Fade();
+			DDCurtain.SetCurtain(30, -1.0);
+
+			foreach (DDScene scene in DDSceneUtils.Create(40))
+			{
+				this.SimpleMenu.DrawWall();
+				DDEngine.EachFrame();
+			}
+
+			GC.Collect();
+		}
+
+		private void ReturnTitleMenu()
+		{
+			Ground.I.Music.Title.Play();
+
+			GC.Collect();
+		}
+	}
+}
