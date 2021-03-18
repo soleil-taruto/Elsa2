2910a9119c45a1a70c80d4abb2442a6298bcd011
soleil-taruto <soleil.taruto@gmail.com>
2021-03-18 18:36:10 +0900

    Voyager 1 is 22777219955.337 kilometers away from the Sun.

--- e20210361_MSSAGame/Elsa20200001/Elsa20200001/Commons/ProcMain.cs
+++ e20210361_MSSAGame/Elsa20200001/Elsa20200001/Commons/ProcMain.cs
@@ -16,7 +16,7 @@ namespace Charlotte.Commons
 	{
 		// APP_IDENT の衝突の解消 -> t20210105_SolveUUIDCollision
 
-		public const string APP_IDENT = "{a4290c96-1955-44a0-8801-cc3fb4a9baa0}"; // アプリ毎に変更する。
+		public const string APP_IDENT = "{38541d94-d8fa-4e44-9476-e08a4a63179e}"; // アプリ毎に変更する。
 		public const string APP_TITLE = "Elsa20200001-Game";
 
 		public static string SelfFile;
