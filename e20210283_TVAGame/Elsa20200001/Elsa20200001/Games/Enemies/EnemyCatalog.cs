66f86c8ce27b738fa2c02e1edf23d9ca6a0d02f7
soleil-taruto <soleil.taruto@gmail.com>
2021-04-05 00:30:52 +0900

    Voyager 1 is 22802405491.592 kilometers away from the Sun.

---
+++ e20210283_TVAGame/Elsa20200001/Elsa20200001/Games/Enemies/EnemyCatalog.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Commons;
+using Charlotte.GameCommons;
+using Charlotte.Games.Enemies.Tests;
+using Charlotte.Games.Enemies.Tests.神奈子s;
+
+namespace Charlotte.Games.Enemies
+{
+	/// <summary>
+	/// 敵のカタログ
+	/// </summary>
+	public static class EnemyCatalog
+	{
+		private class EnemyInfo
+		{
+			public string Name; // 敵の名前、マップ上の配置とか識別に使用する。(開発中、変更してはならない)
+			public string DisplayName; // 表示名(開発中、変更しても良い)
+			public Func<Enemy> Creator;
+
+			public EnemyInfo(string name, Func<Enemy> creator)
+			{
+				int colonIndex = name.IndexOf(':');
+
+				if (colonIndex == -1)
+				{
+					this.Name = name;
+					this.DisplayName = name;
+				}
+				else
+				{
+					this.Name = name.Substring(0, colonIndex);
+					this.DisplayName = name.Substring(colonIndex + 1);
+				}
+				this.Creator = creator;
+			}
+		}
+
+		// Creator 用
+		// -- 初期値は適当な値
+		private static double X = 300.0;
+		private static double Y = 300.0;
+
+		private static EnemyInfo[] Tiles = new EnemyInfo[]
+		{
+			new EnemyInfo(GameConsts.ENEMY_NONE, () => { throw new DDError("敵「無」を生成しようとしました。"); }),
+			new EnemyInfo("スタート地点", () => new Enemy_スタート地点(X, Y, 5)),
+			new EnemyInfo("上から入場", () => new Enemy_スタート地点(X, Y, 8)),
+			new EnemyInfo("下から入場", () => new Enemy_スタート地点(X, Y, 2)),
+			new EnemyInfo("左から入場", () => new Enemy_スタート地点(X, Y, 4)),
+			new EnemyInfo("右から入場", () => new Enemy_スタート地点(X, Y, 6)),
+			new EnemyInfo("ロード地点", () => new Enemy_スタート地点(X, Y, 101)),
+			new EnemyInfo("セーブ地点", () => new Enemy_Bセーブ地点(X, Y)),
+			new EnemyInfo("アイテム・武器-Bounce", () => new Enemy_Bアイテム(X, Y, Enemy_Bアイテム.効用_e.WEAPON_BOUNCE)),
+			new EnemyInfo("アイテム・武器-Spread", () => new Enemy_Bアイテム(X, Y, Enemy_Bアイテム.効用_e.WEAPON_SPREAD)),
+			new EnemyInfo("アイテム・武器-Wave", () => new Enemy_Bアイテム(X, Y, Enemy_Bアイテム.効用_e.WEAPON_WAVE)),
+			new EnemyInfo("敵01", () => new Enemy_B0001(X, Y)),
+			new EnemyInfo("敵02", () => new Enemy_B0002(X, Y)),
+			new EnemyInfo("神奈子", () => new Enemy_B神奈子(X, Y)),
+			new EnemyInfo("イベント0001", () => new Enemy_Bイベント0001(X, Y)),
+			new EnemyInfo("ハック0001", () => new Enemy_Bハック0001(X, Y)),
+
+			// 新しい敵をここへ追加..
+		};
+
+		public static string[] GetNames()
+		{
+			return Tiles.Select(enemy => enemy.Name).ToArray();
+		}
+
+		public static string[] GetDisplayNames()
+		{
+			return Tiles.Select(enemy => enemy.DisplayName).ToArray();
+		}
+
+		public static Enemy Create(string name, double x, double y)
+		{
+			X = x;
+			Y = y;
+
+			return SCommon.FirstOrDie(Tiles, enemy => enemy.Name == name, () => new DDError(name)).Creator();
+		}
+	}
+}
