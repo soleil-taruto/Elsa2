5b85021a3bf631d14f87d54bd35d4e8dbd88c358
soleil-taruto <soleil.taruto@gmail.com>
2021-04-11 00:24:04 +0900

    Voyager 1 is 22811160458.732 kilometers away from the Sun.

---
+++ e20210244_Hakonoko/Elsa20200001/Elsa20200001/LevelEditors/LevelEditor.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.GameCommons;
+using Charlotte.Commons;
+using Charlotte.Games;
+
+namespace Charlotte.LevelEditors
+{
+	/// <summary>
+	/// 編集モードに関する機能
+	/// </summary>
+	public static class LevelEditor
+	{
+		public static LevelEditorDlg Dlg = null;
+
+		public static void ShowDialog()
+		{
+			if (Dlg != null)
+				throw null; // never
+
+			Dlg = new LevelEditorDlg();
+			Dlg.Show();
+		}
+
+		public static void CloseDialog()
+		{
+			Dlg.Close();
+			Dlg.Dispose();
+			Dlg = null;
+		}
+
+		public static void DrawTiles()
+		{
+			int cam_l = DDGround.ICamera.X;
+			int cam_t = DDGround.ICamera.Y;
+			int cam_r = cam_l + DDConsts.Screen_W;
+			int cam_b = cam_t + DDConsts.Screen_H;
+
+			I2Point lt = GameCommon.ToTablePoint(cam_l, cam_t);
+			I2Point rb = GameCommon.ToTablePoint(cam_r, cam_b);
+
+			for (int x = lt.X; x <= rb.X; x++)
+			{
+				for (int y = lt.Y; y <= rb.Y; y++)
+				{
+					MapCell cell = Game.I.Map.GetCell(x, y);
+					I3Color color = new I3Color(0, 0, 0);
+					string name = "";
+
+					switch (cell.Kind)
+					{
+						case MapCell.Kind_e.EMPTY:
+							goto endDraw;
+
+						case MapCell.Kind_e.START:
+							color = new I3Color(0, 255, 0);
+							break;
+
+						case MapCell.Kind_e.GOAL:
+							color = new I3Color(0, 255, 255);
+							break;
+
+						case MapCell.Kind_e.WALL:
+							color = new I3Color(255, 255, 0);
+							break;
+
+						case MapCell.Kind_e.DEATH:
+							color = new I3Color(255, 0, 0);
+							break;
+
+						default:
+							color = new I3Color(128, 128, 255);
+							name = MapCell.Kine_e_Names[(int)cell.Kind];
+
+							if (name.Contains(':'))
+								name = name.Substring(0, name.IndexOf(':'));
+
+							break;
+					}
+
+					{
+						int tileL = x * GameConsts.TILE_W;
+						int tileT = y * GameConsts.TILE_H;
+
+						DDDraw.SetBright(color);
+						DDDraw.DrawRect(
+							Ground.I.Picture.WhiteBox,
+							tileL - cam_l,
+							tileT - cam_t,
+							GameConsts.TILE_W,
+							GameConsts.TILE_H
+							);
+						DDDraw.Reset();
+
+						DDDraw.SetAlpha(0.1);
+						DDDraw.SetBright(new I3Color(0, 0, 0));
+						DDDraw.DrawRect(
+							Ground.I.Picture.WhiteBox,
+							tileL - cam_l,
+							tileT - cam_t,
+							GameConsts.TILE_W,
+							GameConsts.TILE_H / 2
+							);
+						DDDraw.Reset();
+
+						DDDraw.SetAlpha(0.2);
+						DDDraw.SetBright(new I3Color(0, 0, 0));
+						DDDraw.DrawRect(
+							Ground.I.Picture.WhiteBox,
+							tileL - cam_l,
+							tileT - cam_t,
+							GameConsts.TILE_W / 2,
+							GameConsts.TILE_H
+							);
+						DDDraw.Reset();
+
+						DDGround.EL.Add(() =>
+						{
+							DDPrint.SetBorder(new I3Color(0, 64, 64));
+							DDPrint.SetPrint(tileL - cam_l, tileT - cam_t);
+							DDPrint.DebugPrint(name);
+							DDPrint.Reset();
+
+							return false;
+						});
+					}
+
+				endDraw:
+					;
+				}
+			}
+		}
+	}
+}
---
+++ e20210244_Hakonoko/Elsa20200001/Elsa20200001/ResourceMusic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.GameCommons;
+
+namespace Charlotte
+{
+	public class ResourceMusic
+	{
+		public DDMusic Dummy = new DDMusic(@"dat\General\muon.wav");
+
+		public DDMusic Title = new DDMusic(@"dat\夜野ムクロジ\komore-bi.mp3");
+		public DDMusic Novel = new DDMusic(@"dat\夜野ムクロジ\si-piano.mp3");
+		public DDMusic Endinge_死亡 = new DDMusic(@"dat\夜野ムクロジ\sito-sito.mp3");
+		public DDMusic Music_0001 = new DDMusic(@"dat\夜野ムクロジ\yoake.mp3");
+		public DDMusic Music_0002 = new DDMusic(@"dat\夜野ムクロジ\yu-rei.mp3");
+
+		public ResourceMusic()
+		{
+			//this.Dummy.Volume = 0.1; // 非推奨
+		}
+	}
+}
