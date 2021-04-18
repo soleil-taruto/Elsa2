4b2d839ca39f4d14bc2ab9ade642cbc0fc6d7116
soleil-taruto <soleil.taruto@gmail.com>
2021-04-19 02:24:20 +0900

    Voyager 1 is 22822964936.245 kilometers away from the Sun.

--- e20210244_Hakonoko/Elsa20200001/Elsa20200001/ResourceMusic.cs
+++ e20210244_Hakonoko/Elsa20200001/Elsa20200001/ResourceMusic.cs
@@ -15,14 +15,14 @@ namespace Charlotte
 		public DDMusic Ending_死亡 = new DDMusic(true, @"dat\夜野ムクロジ\sito-sito.mp3");
 		public DDMusic Music_0001 = new DDMusic(true, @"dat\夜野ムクロジ\yu-rei.mp3");
 
-		public DDMusic Floor_01 = new DDMusic(false, @"dat\甘茶の音楽工房\orb1.mp3");
-		public DDMusic Floor_02 = new DDMusic(false, @"dat\甘茶の音楽工房\orb2.mp3");
-		public DDMusic Floor_03 = new DDMusic(false, @"dat\甘茶の音楽工房\daremoinaisabaku.mp3");
-		public DDMusic Floor_04 = new DDMusic(false, @"dat\甘茶の音楽工房\natsunokiri.mp3");
-		public DDMusic Floor_05 = new DDMusic(false, @"dat\甘茶の音楽工房\kanashiminotexture2.mp3");
-		public DDMusic Floor_06 = new DDMusic(false, @"dat\甘茶の音楽工房\kanashiminotexture1.mp3");
-		public DDMusic Floor_07 = new DDMusic(false, @"dat\甘茶の音楽工房\moeochirusakura.mp3");
-		public DDMusic Floor_08 = new DDMusic(false, @"dat\甘茶の音楽工房\seijaku2.mp3");
+		public DDMusic Floor_01 = new DDMusic(false, @"dat\甘茶の音楽工房\orb1_muon-100-100.mp3");
+		public DDMusic Floor_02 = new DDMusic(false, @"dat\甘茶の音楽工房\orb2_muon-100-100.mp3");
+		public DDMusic Floor_03 = new DDMusic(false, @"dat\甘茶の音楽工房\daremoinaisabaku_muon-100-100.mp3");
+		public DDMusic Floor_04 = new DDMusic(false, @"dat\甘茶の音楽工房\natsunokiri_muon-100-100.mp3");
+		public DDMusic Floor_05 = new DDMusic(false, @"dat\甘茶の音楽工房\kanashiminotexture2_muon-100-100.mp3");
+		public DDMusic Floor_06 = new DDMusic(false, @"dat\甘茶の音楽工房\kanashiminotexture1_muon-100-100.mp3");
+		public DDMusic Floor_07 = new DDMusic(false, @"dat\甘茶の音楽工房\moeochirusakura_muon-100-100.mp3");
+		public DDMusic Floor_08 = new DDMusic(false, @"dat\甘茶の音楽工房\seijaku2_muon-100-100.mp3");
 		public DDMusic Floor_09 = new DDMusic(false, @"dat\General\muon.wav"); // TODO
 
 		public ResourceMusic()
