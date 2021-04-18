e00a93488c0a022f8b144a5fc48cfb336af4beb7
soleil-taruto <soleil.taruto@gmail.com>
2021-04-18 14:54:31 +0900

    Voyager 1 is 22822265414.522 kilometers away from the Sun.

--- e20210244_Hakonoko/Elsa20200001/Elsa20200001/ResourceMusic.cs
+++ e20210244_Hakonoko/Elsa20200001/Elsa20200001/ResourceMusic.cs
@@ -8,22 +8,22 @@ namespace Charlotte
 {
 	public class ResourceMusic
 	{
-		public DDMusic Dummy = new DDMusic(@"dat\General\muon.wav");
+		public DDMusic Dummy = new DDMusic(true, @"dat\General\muon.wav");
 
-		public DDMusic Title = new DDMusic(@"dat\夜野ムクロジ\komore-bi.mp3");
-		public DDMusic Novel = new DDMusic(@"dat\夜野ムクロジ\si-piano.mp3");
-		public DDMusic Ending_死亡 = new DDMusic(@"dat\夜野ムクロジ\sito-sito.mp3");
-		public DDMusic Music_0001 = new DDMusic(@"dat\夜野ムクロジ\yu-rei.mp3");
+		public DDMusic Title = new DDMusic(true, @"dat\夜野ムクロジ\komore-bi.mp3");
+		public DDMusic Novel = new DDMusic(true, @"dat\夜野ムクロジ\si-piano.mp3");
+		public DDMusic Ending_死亡 = new DDMusic(true, @"dat\夜野ムクロジ\sito-sito.mp3");
+		public DDMusic Music_0001 = new DDMusic(true, @"dat\夜野ムクロジ\yu-rei.mp3");
 
-		public DDMusic Floor_01 = new DDMusic(@"dat\甘茶の音楽工房\orb1.mp3");
-		public DDMusic Floor_02 = new DDMusic(@"dat\甘茶の音楽工房\orb2.mp3");
-		public DDMusic Floor_03 = new DDMusic(@"dat\甘茶の音楽工房\daremoinaisabaku.mp3");
-		public DDMusic Floor_04 = new DDMusic(@"dat\甘茶の音楽工房\natsunokiri.mp3");
-		public DDMusic Floor_05 = new DDMusic(@"dat\甘茶の音楽工房\kanashiminotexture2.mp3");
-		public DDMusic Floor_06 = new DDMusic(@"dat\甘茶の音楽工房\kanashiminotexture1.mp3");
-		public DDMusic Floor_07 = new DDMusic(@"dat\甘茶の音楽工房\moeochirusakura.mp3");
-		public DDMusic Floor_08 = new DDMusic(@"dat\甘茶の音楽工房\seijaku2.mp3");
-		public DDMusic Floor_09 = new DDMusic(@"dat\General\muon.wav"); // TODO
+		public DDMusic Floor_01 = new DDMusic(false, @"dat\甘茶の音楽工房\orb1.mp3");
+		public DDMusic Floor_02 = new DDMusic(false, @"dat\甘茶の音楽工房\orb2.mp3");
+		public DDMusic Floor_03 = new DDMusic(false, @"dat\甘茶の音楽工房\daremoinaisabaku.mp3");
+		public DDMusic Floor_04 = new DDMusic(false, @"dat\甘茶の音楽工房\natsunokiri.mp3");
+		public DDMusic Floor_05 = new DDMusic(false, @"dat\甘茶の音楽工房\kanashiminotexture2.mp3");
+		public DDMusic Floor_06 = new DDMusic(false, @"dat\甘茶の音楽工房\kanashiminotexture1.mp3");
+		public DDMusic Floor_07 = new DDMusic(false, @"dat\甘茶の音楽工房\moeochirusakura.mp3");
+		public DDMusic Floor_08 = new DDMusic(false, @"dat\甘茶の音楽工房\seijaku2.mp3");
+		public DDMusic Floor_09 = new DDMusic(false, @"dat\General\muon.wav"); // TODO
 
 		public ResourceMusic()
 		{
