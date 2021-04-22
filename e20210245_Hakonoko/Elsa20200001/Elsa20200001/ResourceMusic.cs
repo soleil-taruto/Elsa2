d5336b3e6a8d10caa98b4dbdb4779ddc3478f9ad
soleil-taruto <soleil.taruto@gmail.com>
2021-04-22 22:31:48 +0900

    Voyager 1 is 22828570319.230 kilometers away from the Sun.

---
+++ e20210245_Hakonoko/Elsa20200001/Elsa20200001/ResourceMusic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.GameCommons;
+
+namespace Charlotte
+{
+	public class ResourceMusic
+	{
+		public DDMusic Dummy = new DDMusic(true, @"dat\General\muon.wav");
+
+		public DDMusic Title = new DDMusic(true, @"dat\夜野ムクロジ\komore-bi.mp3");
+		public DDMusic Novel = new DDMusic(true, @"dat\夜野ムクロジ\si-piano.mp3");
+		public DDMusic Ending_死亡 = new DDMusic(true, @"dat\夜野ムクロジ\sito-sito.mp3");
+		public DDMusic Music_0001 = new DDMusic(true, @"dat\夜野ムクロジ\yu-rei.mp3");
+
+		public DDMusic Floor_01 = new DDMusic(false, @"dat\甘茶の音楽工房\orb1_muon-100-100.mp3");
+		public DDMusic Floor_02 = new DDMusic(false, @"dat\甘茶の音楽工房\orb2_muon-100-100.mp3");
+		public DDMusic Floor_03 = new DDMusic(false, @"dat\甘茶の音楽工房\daremoinaisabaku_muon-100-100.mp3");
+		public DDMusic Floor_04 = new DDMusic(false, @"dat\甘茶の音楽工房\natsunokiri_muon-100-100.mp3");
+		public DDMusic Floor_05 = new DDMusic(false, @"dat\甘茶の音楽工房\kanashiminotexture2_muon-100-100.mp3");
+		public DDMusic Floor_06 = new DDMusic(false, @"dat\甘茶の音楽工房\kanashiminotexture1_muon-100-100.mp3");
+		public DDMusic Floor_07 = new DDMusic(false, @"dat\甘茶の音楽工房\moeochirusakura_muon-100-100.mp3");
+		public DDMusic Floor_08 = new DDMusic(false, @"dat\甘茶の音楽工房\seijaku2_muon-100-100.mp3");
+		public DDMusic Floor_09 = new DDMusic(false, @"dat\General\muon.wav"); // TODO
+
+		public ResourceMusic()
+		{
+			//this.Dummy.Volume = 0.1; // 非推奨
+		}
+	}
+}
