e5ec3ea25a2a9384ac802c561ac20499b050622e
soleil-taruto <soleil.taruto@gmail.com>
2021-05-02 22:58:18 +0900

    Voyager 1 is 22843199802.209 kilometers away from the Sun.

--- e20210282_Actor8301/Elsa20200001/Elsa20200001/GameCommons/DDEngine.cs
+++ e20210282_Actor8301/Elsa20200001/Elsa20200001/GameCommons/DDEngine.cs
@@ -47,6 +47,7 @@ namespace Charlotte.GameCommons
 			//Ground.EL.ExecuteAllTask();
 
 			DDGround.EL.ExecuteAllTask();
+			DDGround.SystemTasks.ExecuteAllTask();
 			DispDebug();
 			DDMouse.PosChanged_Delay();
 			DDCurtain.EachFrame();
