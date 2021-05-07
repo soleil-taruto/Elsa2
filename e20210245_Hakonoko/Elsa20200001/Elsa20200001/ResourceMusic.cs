1839bfe79d6cbc0f449fe71ceea98ea4173539ea
soleil-taruto <soleil.taruto@gmail.com>
2021-05-07 19:09:01 +0900

    Voyager 1 is 22850268330.586 kilometers away from the Sun.

--- e20210245_Hakonoko/Elsa20200001/Elsa20200001/ResourceMusic.cs
+++ e20210245_Hakonoko/Elsa20200001/Elsa20200001/ResourceMusic.cs
@@ -22,8 +22,8 @@ namespace Charlotte
 		public DDMusic Floor_04 = new DDMusic(@"dat\甘茶の音楽工房\natsunokiri_muon-100-100.mp3");
 		public DDMusic Floor_05 = new DDMusic(@"dat\甘茶の音楽工房\amenoprelude_muon-100-100.mp3");
 		public DDMusic Floor_06 = new DDMusic(@"dat\甘茶の音楽工房\kanashiminotexture1_muon-100-100.mp3");
-		public DDMusic Floor_07 = new DDMusic(@"dat\甘茶の音楽工房\moeochirusakura_muon-100-100.mp3");
-		public DDMusic Floor_08 = new DDMusic(@"dat\甘茶の音楽工房\kanashiminotexture2_muon-100-100.mp3");
+		public DDMusic Floor_07 = new DDMusic(@"dat\甘茶の音楽工房\moeochirusakura_muon-100-100.mp3").SetLoopByStEnd(793000, 3930000);
+		public DDMusic Floor_08 = new DDMusic(@"dat\甘茶の音楽工房\kanashiminotexture2_muon-100-100.mp3").SetLoopByStEnd(654000, 4560000);
 		public DDMusic Floor_09 = new DDMusic(@"dat\ユーフルカ\Horror-naraku_loop.ogg").SetLoopByStLength(974485, 1940413);
 		public DDMusic FinalZone = new DDMusic(@"dat\ユーフルカ\Horror-NeverLookBack_loop.ogg").SetLoopByStLength(456198, 3097689);
 
