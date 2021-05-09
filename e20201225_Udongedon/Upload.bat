d06625c6b90e60c965571a48914efaf3643cedd0
soleil-taruto <soleil.taruto@gmail.com>
2021-05-09 22:11:09 +0900

    Voyager 1 is 22853373789.014 kilometers away from the Sun.

--- e20201225_Udongedon/Upload.bat
+++ e20201225_Udongedon/Upload.bat
@@ -2,8 +2,8 @@ CALL qq
 cx **
 
 rem	CALL DebugRelease.bat /B
-rem	CALL Release.bat /B
-	CALL Release.bat /V 006
+	CALL Release.bat /B
+rem	CALL Release.bat /V 007
 
 C:\Factory\Petra\UpdatingCopy.exe out C:\be\Web\DocRoot\Elsa2\*
 C:\Factory\Petra\RunOnBatch.exe C:\be\Web Upload.bat
