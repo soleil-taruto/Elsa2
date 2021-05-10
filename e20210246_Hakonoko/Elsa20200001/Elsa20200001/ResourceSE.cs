0b9645185298fc1621b3fb0ce0c055e782cd7ea8
soleil-taruto <soleil.taruto@gmail.com>
2021-05-11 02:29:34 +0900

    Voyager 1 is 22855096074.759 kilometers away from the Sun.

--- e20210246_Hakonoko/Elsa20200001/Elsa20200001/ResourceSE.cs
+++ e20210246_Hakonoko/Elsa20200001/Elsa20200001/ResourceSE.cs
@@ -19,8 +19,8 @@ namespace Charlotte
 		public DDSE Reborn = new DDSE(@"dat\効果音ラボ\決定、ボタン押下33.mp3"); // wavVolumeでノイズが入るので止む無くそのままで
 		public DDSE Snapshot = new DDSE(@"dat\効果音ラボ\カメラのシャッター1_wv_[300].mp3");
 
-		public DDSE EnterPause = new DDSE(@"dat\効果音ラボ\決定、ボタン押下34_wv_[300].mp3");
-		public DDSE LeavePause = new DDSE(@"dat\効果音ラボ\決定、ボタン押下35_wv_[300].mp3");
+		public DDSE EnterPause = new DDSE(@"dat\効果音ラボ\決定、ボタン押下34.mp3");
+		public DDSE LeavePause = new DDSE(@"dat\効果音ラボ\決定、ボタン押下35.mp3");
 		public DDSE LeavePause_Title = new DDSE(@"dat\効果音ラボ\決定、ボタン押下22_wv_[300].mp3");
 
 		public ResourceSE()
